aa11d3c784bb886dd3e07fcfd2ad90833ece3223
plmandreza <61202895+plmandreza@users.noreply.github.com>
2025-01-25 12:02:18 -0300

    início javascript

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+console.log('Hello, World!');
+
+// Tipos de dados
+// string = "texto" 'texto' `texto`
+// number = 1 2 3 4 5 6 7 8 9 22.4 3.14
+// boolean = true false
+// array = [1, 2, 3, 4, 5, 6, 7, 8, 9] ['a', 'b', 'c']
+// object =  {
+//      nome: 'Fabio', 
+//      idade: 80, 
+//      cidade: 'Salvador',
+//      maiorDeIdade: true
+// }
+
+// console.log('Fabio');
+var nome = 'Fabio';
+console.log(nome);
